3977ed713cfa79d9847cfdc18533155bac84b44f
Gabriel Mazetto <gabriel.mazetto@resultadosdigitais.com.br>
2015-08-20 18:53:57 -0300

    Update to support more recent lita versions

--- lib/lita-high-five.rb
+++ lib/lita-high-five.rb
@@ -1,1 +1,12 @@
+require "lita"
+
+Lita.load_locales Dir[File.expand_path(
+  File.join("..", "..", "locales", "*.yml"), __FILE__
+)]
+
 require "lita/handlers/high_five"
+
+Lita::Handlers::HighFive.template_root File.expand_path(
+  File.join("..", "..", "templates"),
+ __FILE__
+)
